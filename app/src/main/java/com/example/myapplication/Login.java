bf4965c53f3280eb7e883810d8d80c0344be7eec
ngyihong <danielngyh04@gmail.com>
2023-08-18 00:29:38 +0800

    test comment (added json file)

--- app/src/main/java/com/example/myapplication/Login.java
+++ app/src/main/java/com/example/myapplication/Login.java
@@ -90,7 +90,7 @@ public class Login extends AppCompatActivity {
 
                                 } else {
                                     // If sign in fails, display a message to the user.
-
+                                    // testing
                                     Toast.makeText(Login.this, "Authentication failed.",
                                             Toast.LENGTH_SHORT).show();
 
