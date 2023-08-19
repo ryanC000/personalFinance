bfa3708577bf3e063f50845271ffcb9e1b2b2866
ngyihong <danielngyh04@gmail.com>
2023-08-20 00:53:18 +0800

    home and input page

--- app/src/main/java/com/example/myapplication/Home.java
+++ app/src/main/java/com/example/myapplication/Home.java
@@ -4,19 +4,18 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.renderscript.ScriptGroup;
 import android.view.View;
 
 public class Home extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home2);
-        }
+        setContentView(R.layout.activity_home);
+    }
 
     public void openInputSpending(View view) {
         startActivity(new Intent(this,InputSpending.class));
     }
 }
-
 
